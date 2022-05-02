12fef72f7284fd37724a062c35f7758b2ca44d1d
Latif Doğan Topluoğlu <latiftopluoglu@gmail.com>
2022-05-02 21:51:27 +0300

    .

---
+++ article/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Article
+# Register your models here.
+@admin.register(Article) #admin paneli görselleştirme
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title","author","created_date"]
+    list_display_links = ["title","created_date"]
+    search_fields = ["title"]
+    list_filter = ["created_date"]
+    class Meta: #article admin ile article ı bağliyoruz
+        model = Article
